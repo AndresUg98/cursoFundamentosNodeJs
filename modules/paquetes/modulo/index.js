80c2e20a0428ddc03c2229a3556015e80f9576ee
AndresUg98 <oscaran2109@gmail.com>
2022-01-24 22:23:02 -0700

    update

---
+++ modules/paquetes/modulo/index.js
@@ -0,0 +1,9 @@
+//Traer nuestro modulo
+
+const saludar = require('./modulo');
+const modulo = require('./modulo');
+
+//ejecutar modulo de la funcion
+
+console.log(modulo.prop1);
+modulo.saludar();
